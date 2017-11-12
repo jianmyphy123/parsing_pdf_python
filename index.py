39ffb288933f2033e0dc0b7c0e745590fb6f11b1
jianmyphy123 <jianmyphy123@tutanota.com>
2017-11-13 08:04:47 +1100

    fix bug

--- index.py
+++ index.py
@@ -7,8 +7,12 @@ from pdfminer.pdfpage import PDFPage
 
 import pandas as pd
 import numpy as np
+import re, copy
 
-from scrapy.selector import Selector
+from os import listdir
+from os.path import isfile, join
+
+
 
 
 def convert(fname, pages=None):
@@ -32,58 +36,76 @@ def convert(fname, pages=None):
     output.close
     return text
 
-titleArr = ['Positionskennzahlen']
+mypath = './data'
+titleArr = ['Positionskennzahlen', 'PORTFOLIO CHARACTERISTICS']
 
-text = convert('./data/FSD_LU1495639202_SWC_CH_de.pdf')
-textArr = text.split('\n')
 
-arr = np.array(textArr)
-textIndexArr = np.where(arr=='')[0]
+files = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))]
 
-titleIndex = int(np.where(arr==titleArr[0])[0])
+for filename in files:
 
-tableDataIndexArr_tmp = np.where(textIndexArr>titleIndex)[0]
+    text = convert(filename)
 
-diffVal = 0
-i = 0
-tableDataIndexArr = []
-prevTextIndexVal = 0
-tableDataIndexArr.append(titleIndex + 1)
-for index in tableDataIndexArr_tmp:
-    if i == 0:
-        textIndexVal = int(textIndexArr[index])
-        diffVal = textIndexVal - int(titleIndex) - 1
-        i += 1
-        tableDataIndexArr.append(textIndexVal)
-        prevTextIndexVal = textIndexVal
-        continue
-    textIndexVal = int(textIndexArr[index])
-    if diffVal == (textIndexVal - prevTextIndexVal):
-        tableDataIndexArr.append(textIndexVal)
-        prevTextIndexVal = textIndexVal
-    else:
-        break
+    textArr = text.split('\n')
 
-df = pd.DataFrame()
-i = 0
-tableData = []
-for index in tableDataIndexArr:
-    if i == 0:
-        i += 1
-    elif i == 1:
-        tableData.append(textArr[tableDataIndexArr[0]:tableDataIndexArr[1]-1])
-        i += 1
-    else:
-        tableData.append(textArr[tableDataIndexArr[i-1]+1:tableDataIndexArr[i]])
-        i += 1
+    for i in range(0, len(textArr)):
+        if i < (len(textArr)-1):
+            if re.match(r'[\xc2\a0]', textArr[i]):
+                if textArr[i+1] == '':
+                    del textArr[i]
+                    i -= 1
+            else:
+                textArr[i] = re.sub(r'[\x0c]', '', textArr[i])
 
-df = pd.DataFrame(data=np.array(tableData).transpose())
-print(df)
+    arr = np.array(textArr)
+    for title in titleArr:
+        titleIndex = np.asarray(np.where(arr==title))
+        if titleIndex.size == 0:
+            print('Title not found in '+filename+': ', title)
+            continue
+        titleIndex = int(titleIndex[0])
 
-# print(textIndexArr[0][0], textIndexArr[0][1], textIndexArr[0][2])
-#
-# df = pd.DataFrame({'title': textArr[1:textIndexArr[0][0]], 'value': textArr[textIndexArr[0][1]+1:texttextIndexArr[0][2]]})
-# print(df)
-#
-df.to_csv('output/csvfile.csv', sep=',', index=False, header=False)
-df.to_excel('output/excelfile.xlsx', sheet_name='sheet1', index=False, header=False)
+        textIndexArr = np.where(arr=='')[0]
+
+        tableDataIndexArr_tmp = np.where(textIndexArr>titleIndex)[0]
+
+
+        diffVal = 0
+        i = 0
+        deep = 5
+        tableDataIndexArr = []
+        prevTextIndexVal = 0
+        tableDataIndexArr.append(titleIndex + 1)
+
+        for index in tableDataIndexArr_tmp:
+            if i == 0:
+                textIndexVal = int(textIndexArr[index])
+                diffVal = textIndexVal - int(titleIndex) - 1
+
+                i += 1
+                tableDataIndexArr.append(textIndexVal)
+                prevTextIndexVal = textIndexVal + 1
+            else:
+                textIndexVal = int(textIndexArr[index])
+                if diffVal == (textIndexVal - prevTextIndexVal):
+                    tableDataIndexArr.append(prevTextIndexVal)
+                    tableDataIndexArr.append(textIndexVal)
+                    prevTextIndexVal = textIndexVal + 1
+                else:
+                    prevTextIndexVal = textIndexVal + 1
+            i += 1
+            if i > deep:
+                break
+
+
+
+        tableData = []
+        for i in range(0, len(tableDataIndexArr), 2):
+            tableData.append(textArr[tableDataIndexArr[i]:tableDataIndexArr[i+1]])
+
+
+        df = pd.DataFrame(data=np.array(tableData).transpose())
+        print(df)
+
+        with open('output/csvfile.csv', 'a') as csv:
+            df.to_csv(csv, sep=',', index=False, header=False)
